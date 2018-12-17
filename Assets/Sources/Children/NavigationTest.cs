020f4c88037829a28ba8a901cdec2033df8cd0d6
Olivetti Guillaume <olivetti.guillaume@gmail.com>
2018-12-17 14:47:26 +0100

    Added a child prefab with navigation test scene

---
+++ Assets/Sources/Children/NavigationTest.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class NavigationTest : MonoBehaviour
+{
+    public Camera mainCam = null;
+
+    private NavMeshAgent _navMeshAgent = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && Physics.Raycast(mainCam.ScreenPointToRay(Input.mousePosition), out RaycastHit camToLevelHit))
+        {
+            _navMeshAgent.SetDestination(camToLevelHit.point);
+        }
+    }
+}
