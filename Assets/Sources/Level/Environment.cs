0888c4ba5c4c9246cf11d0a730075f492bf6d6a4
Olivetti Guillaume <olivetti.guillaume@gmail.com>
2018-12-18 10:42:04 +0100

    Navigation test scene with childs going to seats

---
+++ Assets/Sources/Level/Environment.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Environment : MonoBehaviour
+{
+    public Seating seating = null;
+    public Child[] children = new Child[0];
+
+    public void Start()
+    {
+        foreach (Child kid in children)
+            kid.GoSit(seating);
+    }
+}
