181440708cc9bdff4d47435f7b3396f8181678ad
Emilien REGENT <emilien.regent@gmail.com>
2018-12-19 20:51:06 +0100

    Add end screen
    Change-Id: I028644bae8ecc6acaf08e5f73ec39c9e669409b9

---
+++ Assets/Sources/UI/EndController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace La.Cantina.UI
+{
+    public class EndController : MonoBehaviour
+    {
+        // TODO: Reference EndPlayerController for both winner and losers
+
+        private void Start()
+        {
+            // TODO: Call SetPlayer method for each player and disable others
+        }
+
+        private void Update()
+        {
+            // TODO: Check player input to load the start scene to play again
+        }
+    }
+}
---
+++ Assets/Sources/UI/EndPlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Sources.UI
+{
+    public class EndPlayerController : MonoBehaviour
+    {
+        private const string POINTS_FORMAT = "{0} PTS";
+
+        [SerializeField] private Text   _playerNumber   = null;
+        [SerializeField] private Text   _playerPoints   = null;
+        [SerializeField] private Image  _background     = null;
+
+        public void SetPlayer(int number, int points, Color color)
+        {
+            _playerNumber.text = number.ToString();
+            _playerPoints.text  = string.Format(POINTS_FORMAT, points.ToString());
+            _background.color   = color;
+        }
+    }
+}
