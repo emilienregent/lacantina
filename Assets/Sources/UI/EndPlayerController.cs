c5f5ba4622124afcb64edcd372f15bf97d6b13b8
Emilien REGENT <emilien.regent@gmail.com>
2018-12-20 17:37:46 +0100

    Update git st
    Change-Id: Id74ec00fd4b58f78e2d96c8d4d3feeb62af29341

--- Assets/Sources/UI/EndPlayerController.cs
+++ Assets/Sources/UI/EndPlayerController.cs
@@ -5,7 +5,8 @@ namespace La.Cantina.UI
 {
     public class EndPlayerController : MonoBehaviour
     {
-        private const string POINTS_FORMAT = "{0} PTS";
+        private const string POINTS_FORMAT  = "{0} PTS";
+        private const string NAME_FORMAT    = "PLAYER {0}";
 
         [SerializeField] private Text   _playerNumber   = null;
         [SerializeField] private Text   _playerPoints   = null;
@@ -13,7 +14,7 @@ namespace La.Cantina.UI
 
         public void SetPlayer(int number, int points, Color color)
         {
-            _playerNumber.text = number.ToString();
+            _playerNumber.text  = string.Format(NAME_FORMAT, number.ToString());
             _playerPoints.text  = string.Format(POINTS_FORMAT, points.ToString());
             _background.color   = color;
         }
